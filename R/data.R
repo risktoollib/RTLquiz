a05917993b986b3640230d629e4fc1ca5ef628da
risktoollib <pcote@ualberta.ca>
2020-09-24 16:04:41 -0600

    2020 09 24 sp

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' sp500_desc
+#' @description S&P500 components and metadata
+#' @format data frame
+"sp500_desc"
+
+#' sp500_prices
+#' @description S&P500 components monthly adjusted stock price returns
+#' @format data frame
+"sp500_prices"
---
+++ data-raw/DATASET.R
@@ -0,0 +1,29 @@
+# usethis::use_readme_md()
+#usethis::use_package("tidyquant")
+# usethis::use_pipe()
+library(tidyverse)
+library(tidyquant)
+sp500_desc <- tq_index("SP500") %>% dplyr::filter(!stringr::str_detect(symbol,"BRK.B|BF.B|KEYS|WEC|XRAY"))
+sp500_prices <- tidyquant::tq_get(sp500_desc$symbol,
+                                  get  = "stock.prices",
+                                  from = "2008-01-01",
+                                  to = Sys.Date()) %>%
+  stats::na.omit() %>%
+  dplyr::group_by(symbol) %>%
+  dplyr::select(symbol, date, close = adjusted) %>%
+  tidyquant::tq_transmute(select = close, mutate_fun = to.monthly, indexAt = "lastof")
+usethis::use_data(sp500_desc, overwrite = T)
+usethis::use_data(sp500_prices, overwrite = T)
+
+# Global
+devtools::document()
+
+
+
+
+
+
+
+
+
+
